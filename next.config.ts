51a2085b9ab91dd2e11982af100260a2583ee97f
Lazard <DevLazard@proton.me>
2025-09-15 18:43:41 +0200

    Fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  telemetry: false,
-  eslint: {
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn'
-    }
-  },
   /* config options here */
 };
 
