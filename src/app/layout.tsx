dddb5bf8164b15aa96556543f1a93d241e70d151
Lazard <DevLazard@proton.me>
2025-12-26 00:23:33 +0100

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,13 @@ export const metadata: Metadata = {
   creator: 'THERMO PURE',
   publisher: 'THERMO PURE',
   icons: {
-    icon: '/logo.avif',
-    shortcut: '/logo.avif',
-    apple: '/logo.avif',
+    icon: [
+      { url: '/favicon-16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.png', sizes: '48x48', type: 'image/png' },
+    ],
+    shortcut: '/favicon.png',
+    apple: '/favicon.png',
   },
   robots: {
     index: true,
